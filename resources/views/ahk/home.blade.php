c3562ab60b9a4ed9031c9826f22759c38d756b9c
Rizart Dokollari <r.dokollari@gmail.com>
2015-12-08 11:14:52 +0000

    Remove unsued content from home page.

--- resources/views/ahk/home.blade.php
+++ resources/views/ahk/home.blade.php
@@ -168,41 +168,6 @@
         </div>
     </div>
 
-    <div class="container content-sm">
-        <div class="text-center margin-bottom-50">
-            <h2 class="title-v2 title-center">WE ARE UNIFY AGENCY</h2>
-            <p class="space-lg-hor">If you are going to use a
-                <span class="color-green">passage of Lorem Ipsum</span>, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making
-                <span class="color-green">this the first</span> true generator on the Internet.</p>
-        </div>
-
-        <div class="row content-boxes-v4">
-            <div class="col-md-4 md-margin-bottom-40">
-                <i class="pull-left fa fa-lightbulb-o"></i>
-                <div class="content-boxes-in-v4">
-                    <h2>Creative Ideas</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    <a href="#">Learn More</a>
-                </div>
-            </div>
-            <div class="col-md-4 md-margin-bottom-40">
-                <i class="pull-left fa fa-flash"></i>
-                <div class="content-boxes-in-v4">
-                    <h2>Excellent Features</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    <a href="#">Learn More</a>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <i class="pull-left fa fa-compress"></i>
-                <div class="content-boxes-in-v4">
-                    <h2>Fully Responsive</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    <a href="#">Learn More</a>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
 @section('js-implementing-plugins')
     <script type="text/javascript" src="{!! url('assets/plugins/counter/waypoints.min.js') !!}"></script>
