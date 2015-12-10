fe96b6d31fdea29f14e9ffe8a4e4cbc544259128
Rizart Dokollari <r.dokollari@gmail.com>
2015-12-10 07:23:07 +0000

    Add minor translations for home page.

--- resources/views/ahk/home.blade.php
+++ resources/views/ahk/home.blade.php
@@ -41,7 +41,7 @@
                             data-endeasing="Power1.easeIn"
                             data-captionhidden="off"
                             style="z-index: 6">
-                        We promote and support bilateral business between <br> Greece and Germany
+                        {!! trans('ahk.we_promote_and_support_bilateral_business_between_greece_and_germany') !!}
                     </div>
                     <!-- END LAYER -->
 
@@ -57,110 +57,10 @@
                             data-endeasing="Power1.easeIn"
                             data-captionhidden="off"
                             style="z-index: 6">
-                        <a href="#" class="btn-u btn-u-lg re-btn-brd margin-right-5">News</a>
-                        <a href="#" class="btn-u btn-u-lg">Community</a>
+                        <a href="{!! route('health.news') !!}" class="btn-u btn-u-lg re-btn-brd margin-right-5">{!! trans('ahk.news') !!}</a>
+                        <a href="{!! route('companies_path') !!}" class="btn-u btn-u-lg">{!! trans('ahk.community') !!}</a>
                     </div>
                     <!-- END LAYER -->
-                </li>
-                <!-- END SLIDE -->
-
-                <!-- SLIDE -->
-                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="1000" data-title="Slide 2">
-                    <!-- MAIN IMAGE -->
-                    <img src="{!! url('assets/img/sliders/9.jpg') !!}" alt="darkblurbg" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-
-                    <!-- LAYER -->
-                    <div class="tp-caption re-title-v2 sft"
-                            data-x="center"
-                            data-hoffset="0"
-                            data-y="100"
-                            data-speed="1400"
-                            data-start="1000"
-                            data-easing="easeOutBack"
-                            data-endspeed="300"
-                            data-endeasing="easeInQuad"
-                            data-captionhidden="off"
-                            style="z-index: 6">
-                        We're Creative Team
-                    </div>
-
-                    <!-- LAYER -->
-                    <div class="tp-caption re-text-v1 sft"
-                            data-x="center"
-                            data-hoffset="0"
-                            data-y="150"
-                            data-speed="1600"
-                            data-start="2000"
-                            data-easing="Power4.easeOut"
-                            data-endspeed="300"
-                            data-endeasing="Power1.easeIn"
-                            data-captionhidden="off"
-                            style="z-index: 6">
-                        Unify provides key digital services on web and mobile.
-                    </div>
-
-                    <!-- LAYER -->
-                    <div class="tp-caption sfb"
-                            data-x="center"
-                            data-hoffset="0"
-                            data-y="230"
-                            data-speed="1800"
-                            data-start="3000"
-                            data-easing="Power4.easeOut"
-                            data-endspeed="300"
-                            data-endeasing="Power1.easeIn"
-                            data-captionhidden="off"
-                            style="z-index: 6">
-                        <img src="{!! url('assets/img/mockup/white-ipad.png') !!}" alt="">
-                    </div>
-                </li>
-                <!-- END SLIDE -->
-
-                <!-- SLIDE -->
-                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="1000" data-title="Slide 3">
-                    <!-- MAIN IMAGE -->
-                    <img src="{!! url('assets/img/sliders/8.jpg') !!}" alt="darkblurbg" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-
-                    <div class="tp-caption re-title-v2 sft start"
-                            data-x="20"
-                            data-hoffset="0"
-                            data-y="150"
-                            data-speed="1500"
-                            data-start="500"
-                            data-easing="Back.easeInOut"
-                            data-endeasing="Power1.easeIn"
-                            data-endspeed="300">
-                        Welcome To Unify
-                    </div>
-
-                    <div class="tp-caption re-text-v2 sft start"
-                            data-x="20"
-                            data-hoffset="0"
-                            data-y="220"
-                            data-speed="2000"
-                            data-start="1000"
-                            data-easing="Back.easeInOut"
-                            data-endeasing="Power1.easeIn"
-                            data-endspeed="300">
-                        Unify is creative technology company providing
-                        <br> We minimize the gap between technology and its.
-                    </div>
-
-                    <!-- LAYER -->
-                    <div class="tp-caption sft"
-                            data-x="20"
-                            data-hoffset="0"
-                            data-y="320"
-                            data-speed="1600"
-                            data-start="2800"
-                            data-easing="Power4.easeOut"
-                            data-endspeed="300"
-                            data-endeasing="Power1.easeIn"
-                            data-captionhidden="off"
-                            style="z-index: 6">
-                        <a href="#" class="btn-u btn-u-lg re-btn-brd margin-right-5">Read About Us</a>
-                        <a href="#" class="btn-u btn-u-lg">Visit Our Work</a>
-                    </div>
                 </li>
                 <!-- END SLIDE -->
             </ul>
@@ -178,7 +78,6 @@
 @section('js-inline')
 @endsection
 @section('js-page-level')
-    <script type="text/javascript" src="{!! url('assets/js/app.js') !!}"></script>
     <script type="text/javascript" src="{!! url('assets/js/plugins/revolution-slider.js') !!}"></script>
 
     <script type="text/javascript">
