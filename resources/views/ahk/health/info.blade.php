1e50def3017ad6c5cff1243e231465e3fabd59bc
Rizart Dokollari <r.dokollari@gmail.com>
2015-12-17 17:58:21 +0000

    Update info page view according to new requirements.

--- resources/views/ahk/health/info.blade.php
+++ resources/views/ahk/health/info.blade.php
@@ -13,13 +13,10 @@
         <div class="row">
             <div class="col-md-6">
                 <h2 class="title-v2">OUR SERVICES</h2>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-                <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
-                <br> <a href="#" class="btn-u btn-brd btn-brd-hover btn-u-dark">Read More</a>
-                <a href="#" class="btn-u">Purchase Now</a>
+                <p>Short services and benefits description.</p>
             </div>
             <div class="col-md-6">
-                <img class="img-responsive" src="{!! url('assets/img/mockup/mockup1.png') !!}" alt="">
+                <img class="img-responsive" src="http://dummyimage.com/1000x645/000/fff" alt="">
             </div>
         </div>
     </div><!--/container-->
@@ -29,10 +26,8 @@
     <div class="bg-grey">
         <div class="container content-sm">
             <div class="text-center margin-bottom-50">
-                <h2 class="title-v2 title-center">RECENT NEWS</h2>
-                <p class="space-lg-hor">If you are going to use a
-                    <span class="color-green">passage of Lorem Ipsum</span>, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making
-                    <span class="color-green">this the first</span> true generator on the Internet.</p>
+                <h2 class="title-v2 title-center">HEALTH NEWS</h2>
+                <p class="space-lg-hor">Most popular Health care sector news.</p>
             </div>
 
             <div class="row news-v1">
@@ -83,7 +78,7 @@
     <!--=== Service Blocks ===-->
     <div class="container content-sm">
         <div class="text-center margin-bottom-50">
-            <h2 class="title-v2 title-center">OUR SERVICES</h2>
+            <h2 class="title-v2 title-center">Chamber Services</h2>
             <p class="space-lg-hor">If you are going to use a
                 <span class="color-green">passage of Lorem Ipsum</span>, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making
                 <span class="color-green">this the first</span> true generator on the Internet.</p>
@@ -144,35 +139,6 @@
                 </div>
             </div>
         </div>
-
-        <hr>
-
-        <div class="row content-boxes-v4 content-boxes-v4-sm">
-            <div class="col-md-4 md-margin-bottom-40">
-                <i class="pull-left icon-layers"></i>
-                <div class="content-boxes-in-v4">
-                    <h2>Multiple Layouts</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    <a href="#">Learn More</a>
-                </div>
-            </div>
-            <div class="col-md-4 md-margin-bottom-40">
-                <i class="pull-left icon-rocket"></i>
-                <div class="content-boxes-in-v4">
-                    <h2>Ready To Use</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    <a href="#">Learn More</a>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <i class="pull-left icon-support"></i>
-                <div class="content-boxes-in-v4">
-                    <h2>Stunning Support</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    <a href="#">Learn More</a>
-                </div>
-            </div>
-        </div>
     </div>
     <!--=== End Service Blocks ===-->
 
@@ -180,28 +146,22 @@
     <div class="parallax-counter-v2 parallaxBg1" style="background-position: 50% -17px;">
         <div class="container">
             <ul class="row list-row">
-                <li class="col-md-3 col-sm-6 col-xs-12 md-margin-bottom-30">
+                <li class="col-md-4 col-sm-6 col-xs-12 md-margin-bottom-30">
                     <div class="counters rounded">
                         <span class="counter">18298</span>
-                        <h4 class="font-normal">Web Developers</h4>
+                        <h4 class="font-normal">Chamber Members</h4>
                     </div>
                 </li>
-                <li class="col-md-3 col-sm-6 col-xs-12 md-margin-bottom-30">
+                <li class="col-md-4 col-sm-6 col-xs-12 md-margin-bottom-30">
                     <div class="counters rounded">
                         <span class="counter">24583</span>
-                        <h4 class="font-normal">Designers</h4>
+                        <h4 class="font-normal">Site Users</h4>
                     </div>
                 </li>
-                <li class="col-md-3 col-sm-6 col-xs-12 sm-margin-bottom-30">
+                <li class="col-md-4 col-sm-6 col-xs-12 sm-margin-bottom-30">
                     <div class="counters rounded">
                         <span class="counter">37904</span>
-                        <h4 class="font-normal">Open Contests</h4>
-                    </div>
-                </li>
-                <li class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="counters rounded">
-                        <span class="counter">50892</span>
-                        <h4 class="font-normal">Happy Customors</h4>
+                        <h4 class="font-normal">Team behind the Site</h4>
                     </div>
                 </li>
             </ul>
@@ -212,7 +172,7 @@
     <!--=== Cube Portfolio ===-->
     <div class="container content-sm">
         <div class="text-center margin-bottom-30">
-            <h2 class="title-v2 title-center">LATEST PROJECTS</h2>
+            <h2 class="title-v2 title-center">Projects and Workshops</h2>
             <p class="space-lg-hor">If you are going to use a
                 <span class="color-green">passage of Lorem Ipsum</span>, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making
                 <span class="color-green">this the first</span> true generator on the Internet.</p>
