ba1eb15d93937c6f09c908511821085e46483b8a
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-20 16:17:32 +0000

    Ping styleci.

--- app/Ahk/Helpers/Utilities.php
+++ app/Ahk/Helpers/Utilities.php
@@ -3,7 +3,6 @@
  * @author  Rizart Dokollari <r.dokollari@gmail.com>
  * @since   11/12/2015
  */
-
 namespace App\Ahk\Helpers;
 
 /**
@@ -11,38 +10,38 @@ namespace App\Ahk\Helpers;
  */
 class Utilities
 {
-    /**
-     * @param $year
-     *
-     * @return bool|string
-     */
-    public function autoCopyright($year = "auto")
-    {
-        if (strcmp($year, "auto") === 0) {
-            return date('Y');
-        }
+	/**
+	 * @param $year
+	 *
+	 * @return bool|string
+	 */
+	public function autoCopyright($year = "auto")
+	{
+		if (strcmp($year, "auto") === 0) {
+			return date('Y');
+		}
 
-        if (intval($year) == date('Y')) {
-            return $year;
-        }
+		if (intval($year) == date('Y')) {
+			return $year;
+		}
 
-        if (intval($year) < date('Y')) {
-            return "{$year} - " . date('Y');
-        }
+		if (intval($year) < date('Y')) {
+			return "{$year} - " . date('Y');
+		}
 
-        return date('Y');
-    }
+		return date('Y');
+	}
 
-    /**
-     * @param array  $routes
-     * @param string $activeClass
-     * @param string $inactiveClass
-     *
-     * @return string
-     */
-    public function activate(array $routes, $activeClass = 'active', $inactiveClass = '')
-    {
-        return active_class(app('active')->checkRoute($routes), $activeClass, $inactiveClass);
-    }
+	/**
+	 * @param array  $routes
+	 * @param string $activeClass
+	 * @param string $inactiveClass
+	 *
+	 * @return string
+	 */
+	public function activate(array $routes, $activeClass = 'active', $inactiveClass = '')
+	{
+		return active_class(app('active')->checkRoute($routes), $activeClass, $inactiveClass);
+	}
 }
 
